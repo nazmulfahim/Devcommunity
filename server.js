e404ce20b152adf6c08ed2da61e12a4b189c4f91
unknown <bestwebmaster75@gmail.com>
2018-09-16 07:53:01 +0600

    fixing keys import

--- server.js
+++ server.js
@@ -1,49 +1,46 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Keys = require("./config/Keys");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const path = require("path");
-
 const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+const path = require("path");
 
 const app = express();
 
-// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// DB Config
-const db = require("./config/Keys").mongoURI;
-
+//connect to database
 mongoose.Promise = global.Promise;
-// Connect to MongoDB
-mongoose.connect(
-  db,
-  { useNewUrlParser: true }
-);
-// Passport middleware
+mongoose
+  .connect(Keys.mongoURI)
+  .then(() => console.log("MONGODB DATABASE CONNECTED"))
+  .catch(err => console.log(err));
+
+//passport middleware
 app.use(passport.initialize());
 
-// Passport Config
+//passport config
 require("./config/passport")(passport);
 
-// Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-// Server static assets if in production
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
+  //express will serve up the assests
   app.use(express.static("client/build"));
 
+  //express will serve up the index.html file
+  //if it doesn't recognize the route
+  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
+// server runiing
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`server is running on port ${port}`));
