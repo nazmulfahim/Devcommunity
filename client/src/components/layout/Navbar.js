89b9c4ec582da7f90a042f33682e99d62046c85e
unknown <bestwebmaster75@gmail.com>
2018-09-16 15:35:05 +0600

    Allah please

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { logoutUser } from "../../actions/authActions";
-import { clearCurrentProfile } from "../../actions/profileActions";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../actions/authActions';
+import { clearCurrentProfile } from '../../actions/profileActions';
 
 class Navbar extends Component {
   onLogoutClick(e) {
@@ -14,34 +14,6 @@ class Navbar extends Component {
 
   render() {
     const { isAuthenticated, user } = this.props.auth;
-
-    const { profile, loading } = this.props.profile;
-    let navItem;
-
-    if (profile === null || loading) {
-      navItem = "";
-    } else {
-      // Check if logged in user has profile data
-      if (Object.keys(profile).length > 0) {
-        navItem = (
-          <li className="nav-item">
-            <Link className="nav-link" to={`/profile/${profile.handle}`}>
-              <img
-                className="rounded-circle"
-                src={user.avatar}
-                alt={user.name}
-                style={{ width: "25px", marginRight: "5px" }}
-                title="You must have a Gravatar connected to your email to display an image"
-              />{" "}
-              {user.name}
-            </Link>
-          </li>
-        );
-      } else {
-        // User is logged in but has no profile
-        navItem = "";
-      }
-    }
 
     const authLinks = (
       <ul className="navbar-nav ml-auto">
@@ -55,13 +27,19 @@ class Navbar extends Component {
             Dashboard
           </Link>
         </li>
-        {navItem}
         <li className="nav-item">
           <a
             href=""
             onClick={this.onLogoutClick.bind(this)}
             className="nav-link"
           >
+            <img
+              className="rounded-circle"
+              src={user.avatar}
+              alt={user.name}
+              style={{ width: '25px', marginRight: '5px' }}
+              title="You must have a Gravatar connected to your email to display an image"
+            />{' '}
             Logout
           </a>
         </li>
@@ -102,7 +80,7 @@ class Navbar extends Component {
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                 <Link className="nav-link" to="/profiles">
-                  {" "}
+                  {' '}
                   Developers
                 </Link>
               </li>
@@ -117,16 +95,13 @@ class Navbar extends Component {
 
 Navbar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  profile: state.profile,
   auth: state.auth
 });
 
-export default connect(
-  mapStateToProps,
-  { logoutUser, clearCurrentProfile }
-)(Navbar);
+export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(
+  Navbar
+);
